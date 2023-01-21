20874c2bae7c9ba4acd808c9e6f89bc3d5daa9f5
Merry <merrymartin7@gmail.com>
2023-01-21 15:50:12 -0700

    added torontoData

--- Mapping_GeoJSON_Linestrings/js/logic.js
+++ Mapping_GeoJSON_Linestrings/js/logic.js
@@ -4,7 +4,7 @@ console.log("working");
 
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -19,7 +19,7 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 
 // Create a base layer that holds both maps.
 let baseMaps = {
-  Street: streets,
+  Light: light,
   Dark: dark
 };
 
@@ -27,9 +27,9 @@ let baseMaps = {
 // Create the map object with a center and zoom level.
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-  center: [30, 30],
-  zoom: 2,
-  layers: [streets]
+  center: [44.0, -80.0],
+  zoom: 5,
+  layers: [light]
 });
 
 // Pass our map layers into our layers control and add the layers control to the map.
@@ -37,6 +37,8 @@ L.control.layers(baseMaps).addTo(map);
 
 // Accessing the airport GeoJSON URL
 let airportData = "https://raw.githubusercontent.com/MerryMartin/Mapping_Earthquakes/main/majorAirports.json"
+let torontoData = 
+
 
 // Grabbing our GeoJSON data.
 d3.json(airportData).then(function(data) {
